4dee4375c76faacda15e59cd49ef1c8e2c9e15de
Atul Punde <atulpunde970@gmail.com>
2024-09-22 20:34:59 +0530

    Added input field and fetch data from firebase
    - Navbar component- Firebase Contacts App heading with firebase logo.
    - Added basic css using tailwind.
    - Added react-icons dependency and used the plus icon.
    - Also, created input field and styled it using tailwind css.
    - Added logic to fetch data from firebase.

--- Project-5/src/App.jsx
+++ Project-5/src/App.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import NavBar from "./components/NavBar";
+import { FaSearch } from "react-icons/fa";
+import { AiFillPlusCircle } from "react-icons/ai";
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./config/firebase";
 
 const App = () => {
-  return <div className="bg-red-500 flex justify-items-stretch ">App</div>;
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const getContacts = async () => {
+      try {
+        const contactsRef = collection(db, "contacts");
+        const contactsSnapshot = await getDocs(contactsRef);
+        const contactsList = contactsSnapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+        setContacts;
+        contactsList;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getContacts();
+  }, []);
+
+  return (
+    <div className="mx-auto max-w-[370px] px-4">
+      <NavBar />
+      <div className="flex justify-around items-center gap-2">
+        <div className="relative flex items-center flex-grow">
+          <FaSearch className="absolute ml-1 text-3xl text-white" />
+          <input
+            type="text"
+            className="h-10 flex-grow rounded-md border border-white bg-transparent pl-10 text-white"
+          />
+        </div>
+        <AiFillPlusCircle className="cursor-pointer text-5xl text-white" />
+      </div>
+    </div>
+  );
 };
 
 export default App;
