b88af8f4b66017ad775d3692d08ada017fa8da05
Atul Punde <atulpunde970@gmail.com>
2024-09-24 11:27:30 +0530

    Added form
    - Installed formik dependency and created form UI using it.
    - Added functionality to submit the form values and add them into firebase db.

--- Project-5/src/components/AddAndUpdateContact.jsx
+++ Project-5/src/components/AddAndUpdateContact.jsx
@@ -1,11 +1,39 @@
 import React from "react";
 import Modal from "./Modal";
+import { Field, Form, Formik } from "formik";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../config/firebase";
 
 const AddAndUpdateContact = ({ isOpen, onClose }) => {
+  const addContact = async (contact) => {
+    try {
+      const contactRef = collection(db, "contacts");
+      await addDoc(contactRef, contact);
+    } catch (error) {
+      console.log("Error adding contact to db: ", error);
+    }
+  };
   return (
     <div>
       <Modal isOpen={isOpen} onClose={onClose}>
-        Hi
+        <Formik
+          initialValues={{ name: "", email: "" }}
+          onSubmit={(values) => addContact(values)}
+        >
+          <Form className="flex flex-col gap-4">
+            <div className="flex flex-col gap-1">
+              <label htmlFor="name">Name</label>
+              <Field name="name" className="border h-10" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="email">Email</label>
+              <Field type="email" name="email" className="border h-10" />
+            </div>
+            <button className="bg-orange px-3 py-1.5 border self-end">
+              Add Contact
+            </button>
+          </Form>
+        </Formik>
       </Modal>
     </div>
   );
